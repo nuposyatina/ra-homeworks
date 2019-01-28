317a5efc7b5d12883452cba0e0b00e1493e2b23c
Anna Tsukanova <nuposyatina@gmail.com>
2019-01-28 22:26:50 +0300

    Задания 1,2,3 к домашнему заданию State

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,37 +1,34 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
-      active: this.props.options[0],
-      open: false
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      active: this.props.options[0]
     };
-  },
-  handleChange: function (option) {
-    this.setState({
-      active: option
-    });
-  },
-  toggleOpen: function () {
-    this.setState({
-      open: !this.state.open
-    });
-  },
-  render: function () {
+  };
+
+  toggleOpen = () => {
+    this.setState({open: !this.state.open})
+  }
+
+  handleChange = option => {
+    this.setState({active: option})
+  }
+
+  render() {
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
-          <button className={"btn"} onClick={this.toggleOpen} >
+          <button className={"btn"} onClick={this.toggleOpen}>
             <span>Account Settings</span>
             <i className="material-icons">public</i>
           </button>
           <ul className="dropdown">
             {this.props.options.map((option, i) => (
               <li
-                className={option === this.state.active ? "active" : ""}
-                onClick={() => this.handleChange(option)} >
-                  <a href="#">{option}</a>
+                 className={option === this.state.active ? "active" : ""}
+              onClick={() => this.handleChange(option)} >
+                    <a href="#">{option}</a>
               </li>
             ))}
           </ul>
@@ -39,4 +36,4 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
