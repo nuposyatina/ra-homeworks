1cecefe7874a524bd2de2ef01fbe0826214e5239
Anna Tsukanova <at@indotech.ru>
2019-02-07 11:33:08 +0300

    Роутинг в одностраничном приложении

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,26 @@
 
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+
+	render() {
+		return (
+			<Router>
+				<div className="tabs">
+					<nav className="tabs__items">
+						<NavLink className="tabs__item" to="/" exact activeClassName="tabs__item-active">Рефераты</NavLink>
+						<NavLink className="tabs__item" to="/creator" activeClassName="tabs__item-active">Криэйтор</NavLink>
+						<NavLink className="tabs__item" to="/fortune" activeClassName="tabs__item-active">Гадалка</NavLink>
+					</nav>
+					<div className="tabs__content">
+						<Route exact path="/" component={Essay} />
+						<Route path="/creator" component={Creator} />
+						<Route path="/fortune" component={Fortune} />
+					</div>
+				</div>
+			</Router>
+		)
+	}
+}
